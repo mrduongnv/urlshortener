d8a678ff88a69a6aa11b637839232d72ae82828d
Duong Nguyen <mrduongnv@gmail.com>
2015-07-26 06:41:06 -0400

    Fix bug 2

--- app/models/shortener.rb
+++ app/models/shortener.rb
@@ -1,5 +1,5 @@
 class Shortener < ActiveRecord::Base
-	att_accessible :long, :user_id
+	attr_accessible :long, :user_id
 	belongs_to :user
 	validates :short, :length => { :maximum => 10}
 	validates :long, :length => { :maximum => 250}
