e12b421c3370116bb7b5451ba2218912058882c8
Duong Nguyen <mrduongnv@gmail.com>
2015-07-26 06:39:15 -0400

    New 2

--- app/models/shortener.rb
+++ app/models/shortener.rb
@@ -1,5 +1,5 @@
 class Shortener < ActiveRecord::Base
-	accessible :long, :user_id
+	att_accessible :long, :user_id
 	belongs_to :user
 	validates :short, :length => { :maximum => 10}
 	validates :long, :length => { :maximum => 250}
