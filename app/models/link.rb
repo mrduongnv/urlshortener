403e1e993a9005bc5472ec4865c50d791af81e91
Duong Nguyen <mrduongnv@gmail.com>
2015-07-26 07:47:00 -0400

    New App

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,7 +3,9 @@ class Link < ActiveRecord::Base
   after_create :generate_slug, :screenshot_scrape
 
   def generate_slug
-    self.slug = self.id.to_s(36)
+    o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+    string = (0...8).map { o[rand(o.length)] }.join
+    self.slug = string#self.id.to_s(36)
     self.save
   end
 
