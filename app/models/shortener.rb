5c12b28bfb881ace7f314f90bee478abb954ece9
Duong Nguyen <mrduongnv@gmail.com>
2015-07-26 06:11:05 -0400

    Fix bug

--- app/models/shortener.rb
+++ app/models/shortener.rb
@@ -1,5 +1,5 @@
 class Shortener < ActiveRecord::Base
-	belong_to: user
-	validates: short, length => { :maximum 10}
-	validates: long, length => { :maximum 250}
+	belongs_to :user
+	validates :short, :length => { :maximum => 10}
+	validates :long, :length => { :maximum => 250}
 end
