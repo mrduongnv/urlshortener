38daf291ab669649b4c3e05cc67f7f0c2fbe1f3c
Duong Nguyen <mrduongnv@gmail.com>
2015-07-28 01:04:06 -0400

    New Filter Feature

--- db/migrate/20140222041848_create_links.rb
+++ db/migrate/20140222041848_create_links.rb
@@ -3,6 +3,7 @@ class CreateLinks < ActiveRecord::Migration
     create_table :links do |t|
       t.string :given_url
       t.string :slug
+      t.string :_slug
       t.integer :clicks,  :default => 0
 
       t.timestamps
